8a7c98c1c8d9b2068a76e8b1cd097ba25dc20f4c
Kidus Wubet <kidus.wubet@gmail.com>
2022-05-02 16:33:49 +0200

    Initial commit

---
+++ src/main/java/com/ragnarok/connect/RagnarokApplication.java
@@ -0,0 +1,13 @@
+package com.ragnarok.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagnarokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagnarokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ragnarok/connect/RagnarokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ragnarok.connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagnarokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
